8faaf09eefc238f868d36a656b66981ced36f43f
Julia Clepf <jumgc09@gmail.com>
2021-07-08 19:29:43 -0300

    style: add line to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,16 @@
 import { HalfMoon, Container, DevJobs, Links } from "./styles";
-import Button from "../Button";
+
 const Header = () => {
   return (
     <Container>
-      <Links>Resume</Links>
+      <Links>For Companies</Links>
+
       <HalfMoon>
         <DevJobs>
           <h1>DevJobs</h1>
         </DevJobs>
       </HalfMoon>
-      <div>
-        <Button setColor="red" size="large">
-          <h3>Logout</h3>
-        </Button>
-      </div>
+      <Links>For Developers</Links>
     </Container>
   );
 };
--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -2,18 +2,19 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const Links = styled(Link)`
-  font-size: 1.2rem;
+  font-size: 1rem;
   font-family: "Carrois Gothic", sans-serif;
   text-decoration: none;
   color: var(--dark-grey);
+  text-align: center;
   :hover {
     border-bottom: 1px solid var(--blue);
   }
 `;
 export const HalfMoon = styled.div`
-  background-color: #ececec;
+  background-color: var(--white);
 
-  z-index: -1;
+  z-index: 1;
   text-align: center;
   border-radius: 50% / 0 0 100% 100%;
   width: 50%;
@@ -28,13 +29,26 @@ export const Container = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: space-evenly;
+  background-color: #ececec;
+  z-index: -1;
   padding-right: 4px;
   padding-left: 4px;
+
+  position: relative;
   Link {
     text-decoration: none;
   }
   h3 {
-    font-size: 15px;
+    font-size: 1.2rem;
+  }
+  :after {
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 10px;
+    width: 100vw;
+    height: 1px;
+    background-color: var(--grey);
   }
 `;
 
@@ -48,7 +62,7 @@ export const DevJobs = styled.div`
   position: absolute;
   z-index: 1;
   border: 6px double var(--white);
-  margin-top: 35px;
+  margin-top: 78px;
   h1 {
     margin-top: 35px;
     font-size: 1.2rem;
