0ad8bf6ae1af0a803a3047b4cbea4d208ea69783
Bruno Gouveia <brunovcg@gmail.com>
2021-07-13 20:25:20 +0000

    Revert "Merge branch 'feature/resume' into 'developer'"
    This reverts merge request !69

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -10,7 +10,6 @@ import CompanyRegister from "../pages/CompanyRegister";
 import Messages from "../pages/Messages";
 import VisualizationResume from "../pages/ResumeVisualization";
 import CompanyLogin from "../pages/CompanyLogin";
-import Resume from "../pages/Resume";
 
 const Routes = () => {
   return (
@@ -22,7 +21,6 @@ const Routes = () => {
       <Route path="/companyRegister" exact component={CompanyRegister} />
       <Route path="/companyLogin" exact component={CompanyLogin} />
       <Route path="/messages" exact component={Messages} />
-      <Route path="/resume" exact component={Resume} />
       <Route
         path="/visualizationResume"
         exact
